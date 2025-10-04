5652b58407b1f47a920e8aa1172bb1ba7ba1f984
Lumaura <139267105+Lumaura@users.noreply.github.com>
2025-10-04 14:21:48 -0400

    Made the  shooter game

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShootStrong.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShootStrong.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class HW2PlayerShoot : MonoBehaviour
+public class HW2PlayerShootStrong : MonoBehaviour
 {
     public GameObject preFab;
     public Transform bulletTrash;
     public Transform bulletSpawn;
 
-    private const float Timer = 0.5f;
-    private float _currentTime = 0.5f;
+    private const float Timer = 1.0f;
+    private float _currentTime = 1.0f;
     private bool _canShoot = true;
 
     private void Update()
@@ -32,11 +32,11 @@ public class HW2PlayerShoot : MonoBehaviour
 
     private void Shoot()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _canShoot)
         {
-            GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+            GameObject red = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
 
-            bullet.transform.SetParent(bulletTrash);
+            red.transform.SetParent(bulletTrash);
 
             _canShoot = false;
         }
