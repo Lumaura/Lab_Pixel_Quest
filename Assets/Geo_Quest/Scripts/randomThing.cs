855423bb095c9c521c8a4c9df3acda64d1fe22d5
Lumaura <139267105+Lumaura@users.noreply.github.com>
2025-10-04 10:00:43 -0400

    Another hello world for some reason

--- Assets/Geo_Quest/Scripts/randomThing.cs
+++ Assets/Geo_Quest/Scripts/randomThing.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class randomThing : MonoBehaviour
 {
     int thing = 3;
+    string variable1 = "Hello";
     // Start is called before the first frame update
     void Start()
     {
-        
+        string variable2 = " World";
+        Debug.Log(variable1 + variable2);
     }
 
     // Update is called once per frame
