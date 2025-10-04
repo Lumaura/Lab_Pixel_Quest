5a956a29fb9e03cd259ade18c9c13a1a04381c17
Lumaura <139267105+Lumaura@users.noreply.github.com>
2025-10-04 10:17:50 -0400

    Ball has zoomies
    Yippee

--- Assets/Geo_Quest/Scripts/randomThing.cs
+++ Assets/Geo_Quest/Scripts/randomThing.cs
@@ -4,19 +4,21 @@ using Unity.Burst.Intrinsics;
 using UnityEngine;
 public class randomThing : MonoBehaviour
 {
-    int thing = 3;
+    float thing = 1;
     string variable1 = "Hello";
     // Start is called before the first frame update
     void Start()
     {
         string variable2 = " World";
         Debug.Log(variable1 + variable2);
+        Debug.Log(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        thing++;
-        Debug.Log(thing);
+        thing *= (float)1.01;
+        Debug.Log(transform.position);
+        transform.position = new Vector3(thing, thing, thing);
     }
 }
